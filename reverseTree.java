ab14132097aac48dfa0e9322f92424fb1151b2c4
hoshino13 <52819122+hoshino0113@users.noreply.github.com>
2025-12-21 11:13:09 -0500

    add reverse tree and its notes

---
+++ reverseTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//Becareful that we need to compute the left and right subtrees first before swapping them.
+//otherwise we would lose the references to the original left and right subtrees.
+//or we will end up in swapping the wrong reference!
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
