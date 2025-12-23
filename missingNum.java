0e998d9805cb98e111770e9e1f89b996009dc2cc
hoshino13 <52819122+hoshino0113@users.noreply.github.com>
2025-12-22 19:17:03 -0500

    Update missingNum.java

--- missingNum.java
+++ missingNum.java
@@ -1,4 +1,4 @@
-// we can sort the array and go through it one by one, in O(log(n)) time
+// we can sort the array and go through it one by one, in O(nlog(n)) time
 // however, we iterate through the entire array, and while we do that, we:
 // - Add up i to a variable a, and add up nums[i] to another variable b.
 // - once we hit the last iteration, we simply substract b from a to get the result
