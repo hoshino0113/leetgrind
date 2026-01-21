715b318a5f25aa7b0c78c946f6f50358b6b5b450
hoshino13 <52819122+hoshino0113@users.noreply.github.com>
2026-01-21 15:22:37 -0500

    Update uniquePath.java

--- uniquePath.java
+++ uniquePath.java
@@ -1,3 +1,12 @@
+//there might be another way to do it which requires less space
+// In this question, we are asked to count the number of unique paths from the top-left corner to the bottom-right corner of an m x n grid.
+//Core idea: Use DP
+//- Let dp[i][j] represent the number of unique paths to reach cell (i, j).
+//- To reach cell (i, j), we can come from either the cell directly above it (i-1, j) or the cell directly to the left of it (i, j-1).
+//- Therefore, the number of unique paths to reach cell (i, j) is the sum of the number of unique paths to reach the cell above it and the cell to the left of it.
+//- The base case is the starting cell (1, 1), which has exactly 1 unique path to itself.
+// 
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
